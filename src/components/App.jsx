dfc1a20e2e21075f04e2d41fdeeecfb58ee99048
yurchykyur <yurchyk.yur.s@gmail.com>
2023-06-20 11:58:12 +0300

    revert commit task-03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
-import FriendListItem from './FriendList/FriendListItem/FriendListItem';
 
 import user from '../user.json';
 import data from '../data.json';
-import friends from '../friends.json';
 
 export const App = () => {
   return (
@@ -22,9 +19,6 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-      <FriendList>
-        <FriendListItem friends={friends} />
-      </FriendList>
     </div>
   );
 };
--- src/components/FriendList/FriendList.jsx
+++
@@ -1,5 +0,0 @@
-import css from './FriendList.module.css';
-
-export default function FriendList({ children }) {
-  return <ul className={css.friendList}>{children}</ul>;
-}
