542bb72c06d2d541fbdf7e4c6a2e0e01678a0408
yurchykyur <yurchyk.yur.s@gmail.com>
2023-06-20 11:38:40 +0300

    task-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   return (
@@ -14,7 +16,9 @@ export const App = () => {
         followers={user.stats.followers}
         views={user.stats.views}
         likes={user.stats.likes}
-      />{' '}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import css from './Statisticst.module.css';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.item}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
